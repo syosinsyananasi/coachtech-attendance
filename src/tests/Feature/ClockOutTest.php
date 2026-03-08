7efa52b63094028a812625bdd845d94431fdfdf5
nanashi <cibicco323@gmail.com>
2026-03-08 13:02:18 +0900

    テストファイル作成

---
+++ src/tests/Feature/ClockOutTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ClockOutTest extends TestCase
+{
+    use RefreshDatabase;
+}
---
+++ src/tests/Feature/UserAttendanceListTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserAttendanceListTest extends TestCase
+{
+    use RefreshDatabase;
+}
