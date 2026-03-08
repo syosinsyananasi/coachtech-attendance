44798318dc85a64c592f1924b8334c4427b3f803
nanashi <cibicco323@gmail.com>
2026-03-08 13:54:46 +0900

    日時取得機能 テスト

--- src/tests/Feature/DateTimeTest.php
+++ src/tests/Feature/DateTimeTest.php
@@ -2,10 +2,24 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class DateTimeTest extends TestCase
 {
     use RefreshDatabase;
+
+    /** 現在の日時情報がUIと同じ形式で出力されている */
+    public function test_current_datetime_is_displayed_on_stamp_page()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get('/attendance');
+
+        $response->assertStatus(200);
+        $response->assertSee('id="current-date"', false);
+        $response->assertSee('id="current-time"', false);
+        $response->assertSee('attendance-clock.js', false);
+    }
 }
